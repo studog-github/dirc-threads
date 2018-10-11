5b85d471c4683c00c2e9d0dd545dfe99036107be
Stuart MacDonald <stuartm.coding@gmail.com>
2018-10-11 18:06:12 -0400

    Remove obsolete comment
    Scrolling was fixed by using the object form of the call and scrolling to
    'center'

--- fg_threadHandler.js
+++ fg_threadHandler.js
@@ -2,8 +2,6 @@
 
 {
     let current_page = window.location.href;
-
-    // TODO: Don't scroll if the message is the first in the thread
     let message_url = current_page.split("&")[0];
     let href_attr = message_url.split("/")[4];
     let anchors = $("a[href='" + href_attr + "']");
