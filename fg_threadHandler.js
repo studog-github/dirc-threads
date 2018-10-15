538196c042def869f4c5731ccbe9017271a40521
Stuart MacDonald <stuartm.coding@gmail.com>
2018-10-15 12:36:43 -0400

    Code cleanup for clarity

--- fg_threadHandler.js
+++ fg_threadHandler.js
@@ -4,8 +4,8 @@
     let current_page = window.location.href;
     let message_url = current_page.split("&")[0];
     let href_attr = message_url.split("/")[4];
-    let anchors = $("a[href='" + href_attr + "']");
+    let anchor = $("a[href='" + href_attr + "']")[0];
 
     // the object forms' defaults are { block: "center" } which is desired
-    anchors[0].scrollIntoView({});
+    anchor.scrollIntoView({});
 }
