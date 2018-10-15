0ea133859f6b4e516b0c53305930e1925b16b019
Stuart MacDonald <stuartm.coding@gmail.com>
2018-10-15 13:18:22 -0400

    Select messages and add quote items
    - selects all the message hrefs in the thread
    - adds a Reply and Quote item into the DOM
    
    TODO: Spacing needs improvement
    TODO: Add inline quoting system

--- fg_threadHandler.js
+++ fg_threadHandler.js
@@ -8,4 +8,17 @@
 
     // the object forms' defaults are { block: "center" } which is desired
     visited_message.scrollIntoView({});
+
+    let message_hrefs = href_attr.split("=")[0] + "=";
+    // First two are Next/Prev navigation items, last two are same repeated at bottom of page
+    let anchors = $("a[href^='" + message_hrefs + "']").slice(2,-2);
+
+    for (let a of anchors) {
+        let p = a.parentElement;
+        let new_text_reply = document.createTextNode("Reply");
+        let new_text_quote = document.createTextNode("Quote");
+
+        p.insertBefore(new_text_reply, null);
+        p.insertBefore(new_text_quote, null);
+    }
 }
