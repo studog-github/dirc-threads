e4e5483146a9d714090782a1ee552435604c8f62
Stuart MacDonald <stuartm.coding@gmail.com>
2018-10-15 21:24:37 -0400

    Scroll into view, last
    Make the scroll into view the last action

--- fg_threadHandler.js
+++ fg_threadHandler.js
@@ -6,9 +6,6 @@
     let href_attr = message_url.split("/")[4];
     let visited_message = $("a[href='" + href_attr + "']")[0];
 
-    // the object forms' defaults are { block: "center" } which is desired
-    visited_message.scrollIntoView({});
-
     let message_hrefs = href_attr.split("=")[0] + "=";
     // First two are Next/Prev navigation items, last two are same repeated at bottom of page
     let anchors = $("a[href^='" + message_hrefs + "']").slice(2,-2);
@@ -21,4 +18,7 @@
         p.insertBefore(new_text_reply, null);
         p.insertBefore(new_text_quote, null);
     }
+
+    // the object forms' defaults are { block: "center" } which is desired
+    visited_message.scrollIntoView({});
 }
