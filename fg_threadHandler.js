eb428fadf1453d059340fa90e31ee646fb72f8d7
Stuart MacDonald <stuartm.coding@gmail.com>
2018-10-17 12:54:00 -0400

    Very early work on inline quoted reply
    Adds a reply link and a note for later development

--- fg_threadHandler.js
+++ fg_threadHandler.js
@@ -6,17 +6,19 @@
     let href_attr = message_url.split("/")[4];
     let visited_message = $("a[href='" + href_attr + "']")[0];
 
+// probably need to add a hidden form for the reply post here
+
     let message_hrefs = href_attr.split("=")[0] + "=";
     // First two are Next/Prev navigation items, last two are same repeated at bottom of page
     let anchors = $("a[href^='" + message_hrefs + "']").slice(2,-2);
 
     for (let a of anchors) {
         let p = a.parentElement;
-        let new_text_reply = document.createTextNode("Reply");
-        let new_text_quote = document.createTextNode("Quote");
+        let new_a = document.createElement('a');
+        let new_text_reply = document.createTextNode(" [ Reply ]");
 
-        p.insertBefore(new_text_reply, null);
-        p.insertBefore(new_text_quote, null);
+        new_a.appendChild(new_text_reply);
+        p.insertBefore(new_a, null);
     }
 
     // the object forms' defaults are { block: "center" } which is desired
